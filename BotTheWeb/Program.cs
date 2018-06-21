ae8d7d0ae9e7324ef51e15440cb1ce192e6e5622
surekhasrinivasan <surekhasrao@gmail.com>
2018-06-21 17:07:35 -0400

    Example scraper project

---
+++ BotTheWeb/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace BotTheWeb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Options = new ChromeOptions();
+            Options.AddArguments("--disable-gpu");
+
+            var chromeDriver = new ChromeDriver();
+            chromeDriver.Navigate().GoToUrl("https://www.google.com");
+
+            chromeDriver.FindElementByXPath("//*[@id=\"lst-ib\"]").Click();
+            chromeDriver.Keyboard.SendKeys("books");
+            chromeDriver.Keyboard.SendKeys(Keys.Enter);
+        }
+        
+    }
+}
